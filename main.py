d9d302930a25d59df286db6362456e3875f165e1
Shubham Patrick <ahthneeuhl@gmail.com>
2023-10-08 12:41:52 +0530

    feat: fetch all todos from db

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
-import models
-from db import engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import Annotated
+from models import Todo, Base
+from db import engine, SessionLocal
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todo).all()
