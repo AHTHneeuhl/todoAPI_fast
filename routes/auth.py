a90311a41a5b6462038edfe00f325811933c9175
Shubham Patrick <ahthneeuhl@gmail.com>
2023-10-14 12:53:31 +0530

    feat: encode jwt token

--- routes/auth.py
+++ routes/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from passlib.context import CryptContext
@@ -7,8 +8,12 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 router = APIRouter()
+
+SECRET_KEY = "ZIk9hGJC3jx+BTVU5YbOyVUINppJHwP0RqWZO6BRCxQ="
+ALGORITHM = "HS256"
 
 bcrypt_ctx = CryptContext(schemes=["bcrypt"])
 
@@ -33,13 +38,25 @@ class UserRequest(BaseModel):
     role: str
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 def authenticate_user(username: str, password: str, db):
     user = db.query(User).filter(User.username == username).first()
     if not user:
         return False
     if not bcrypt_ctx.verify(password, user.hashed_password):
         return False
-    return True
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
@@ -57,11 +74,12 @@ async def create_user(db: db_dependency, user_request: UserRequest):
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return "Failed Authentication"
-    return "Successful Authentication"
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {"access_token": token, "token_type": "Bearer"}
